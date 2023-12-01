2b04c0719b72397d43a6f2f702b03e9b1ae48aa0
Simone Tasca <simo.tasca@gmail.com>
2023-12-01 13:51:55 +0100

    more routes

--- website/src/lib/shared/routes.ts
+++ website/src/lib/shared/routes.ts
@@ -9,8 +9,16 @@ const routes = {
   brand: (name: string) => `/brand/` + encodeQueryParam(name.toLowerCase()),
   category: (name: string) =>
     `/category/${encodeQueryParam(name.toLowerCase())}`,
+  type: (category: string, name: string) =>
+    `/category/${encodeQueryParam(category.toLowerCase())}/${encodeQueryParam(
+      name.toLowerCase()
+    )}`,
+  /** TODO: manage filters */
+  products: () => "/products",
   product: (product: UrlProduct) =>
-    `/products/${encodeQueryParam(productName(product))}-${product.id}`,
+    `/products/${encodeQueryParam(productName(product).toLowerCase())}-${
+      product.id
+    }`,
 } as const;
 
 export default routes;
