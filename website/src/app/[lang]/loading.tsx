2ff449b28ef6c30f983fb6b636023d06e219e714
Simone Tasca <simo.tasca@gmail.com>
2023-12-02 14:55:47 +0100

    loading screen

--- website/src/app/[lang]/loading.tsx
+++ website/src/app/[lang]/loading.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import logo from "@/images/logo-dark.svg";
+
 export default function Loading() {
   return (
-    <div className="bg-white w-screen h-screen grid place-content-center text-9xl font-oswald">
-      CARICAMENTO
+    <div className="bg-white w-screen h-screen grid place-content-center">
+      <p className="text-[min(10rem,10vw)] font-oswald -translate-y-10 text-neutral-600 leading-[1]">
+        <span>CARICA</span>
+        <div className="relative text-red-500 inline-block">
+          <span>M</span>
+        </div>
+        <span>ENTO</span>
+      </p>
+      <Image
+        src={logo}
+        alt="logo 2emme2 autoricambi"
+        className="fixed bottom-4 right-4 w-14"
+      />
     </div>
   );
 }
