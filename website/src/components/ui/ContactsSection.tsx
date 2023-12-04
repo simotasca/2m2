617526b9bd8b89d9da4709c31f6d1f824249c8d3
emanuele-hazan <emanuele.hazan@gmail.com>
2023-12-04 18:53:42 +0100

    menu cellulare fatto

--- website/src/components/ui/ContactsSection.tsx
+++ website/src/components/ui/ContactsSection.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import iconEmail from "@/images/icons/payment.svg";
+import iconEmail from "@/images/icons/mail-red.svg";
+import iconPhone from "@/images/icons/phone-red.svg";
+import iconWhatsapp from "@/images/icons/whatsapp.svg";
 import iconTime from "@/images/icons/time.svg";
 import Button from "@/components/ui/Button";
 import MaxWidthContainer from "@/components/ui/MaxWidthContainer";
 
 export default function ContactsSection() {
   return (
-    <div className="grid grid-cols-2 gap-y-6 gap-x-12">
+    <div className="grid md:grid-cols-[3fr_2fr] lg:grid-cols-2 gap-y-6 gap-x-12 max-md:px-1">
       <div>
         <h3 className="text-2xl font-bold mb-1">Come possiamo aiutarti?</h3>
         <p className="leading-tight">
@@ -15,11 +17,11 @@ export default function ContactsSection() {
         </p>
       </div>
       <br />
-      <div className="pl-6">
+      <div className="md:pl-6">
         <ul className="grid grid-cols-[auto_1fr_auto] gap-x-4 gap-y-2 items-center">
           <li className="contents">
-            <Image src={iconEmail} alt=""></Image>
-            <div className="leading-tight">
+            <Image className="max-xs:hidden" src={iconEmail} alt=""></Image>
+            <div className=" leading-tight">
               <p>
                 <span className="text-sm leading-tight">Email: </span>
                 <span className="font-medium leading-tight">
@@ -30,16 +32,17 @@ export default function ContactsSection() {
                 Ti risponderemo a breve
               </p>
             </div>
-            <Button className="w-full uppercase bg-orange-500 text-white rounded-full px-10 py-1.5">
+            <Button className="w-full max-xs:max-w-[30vw] uppercase bg-orange-500 text-white max-xs:text-sm rounded-full px-10 py-1.5">
               Scrivici
             </Button>
           </li>
           <li className="col-span-3 h-px bg-orange-200"></li>
           <li className="contents">
-            <Image src={iconEmail} alt=""></Image>
-            <div className="leading-tight">
+            <Image className="max-xs:hidden" src={iconPhone} alt=""></Image>
+            <div className=" leading-tight">
               <p>
                 <span className="text-sm leading-tight">Telefono: </span>
+                <br className="xs:hidden md:block lg:hidden" />
                 <span className="font-medium leading-tight">
                   +39 374 9284720
                 </span>
@@ -48,16 +51,17 @@ export default function ContactsSection() {
                 Parla con un operatore
               </p>
             </div>
-            <Button className="w-full uppercase bg-orange-500 text-white rounded-full px-10 py-1.5">
+            <Button className="w-full max-xs:max-w-[30vw] uppercase bg-orange-500 text-white max-xs:text-sm rounded-full px-10 py-1.5">
               Chiama
             </Button>
           </li>
           <li className="col-span-3 h-px bg-orange-200"></li>
           <li className="contents">
-            <Image src={iconEmail} alt=""></Image>
-            <div className="leading-tight">
+            <Image className="max-xs:hidden" src={iconWhatsapp} alt=""></Image>
+            <div className=" leading-tight">
               <p>
                 <span className="text-sm leading-tight">Whatsapp: </span>
+                <br className="xs:hidden md:block lg:hidden" />
                 <span className="font-medium leading-tight">
                   +39 374 9284720
                 </span>
@@ -66,16 +70,17 @@ export default function ContactsSection() {
                 Ti risponderemo appena possibile
               </p>
             </div>
-            <Button className="w-full uppercase bg-orange-500 text-white rounded-full px-10 py-1.5">
+            <Button className="w-full max-xs:max-w-[30vw] uppercase bg-orange-500 text-white max-xs:text-sm rounded-full px-6 xs:px-10 py-1.5 ">
               CHAT
             </Button>
           </li>
         </ul>
       </div>
       <div>
-        <p className="font-bold -mt-4 mb-5">
+        <p className="font-bold mt-2 md:-mt-4 mb-5 max-xs:max-w-[85vw]">
           Siamo disponibili nei seguenti orari:
         </p>
+
         <ul className="flex flex-col gap-2">
           <li className="flex gap-3 items-center">
             <Image
@@ -88,7 +93,8 @@ export default function ContactsSection() {
               <span> a </span>
               <b className="font-medium">Venerdì</b>
               <span className="text-neutral-600">
-                : 08:30-12:30 / 14:40-17:30
+                :<br className="xs:hidden md:block lg:hidden" /> 08:30-12:30 /
+                14:40-17:30
               </span>
             </p>
           </li>
@@ -100,7 +106,9 @@ export default function ContactsSection() {
             />
             <p>
               <b className="font-medium">Sabato</b>
-              <span className="text-neutral-600">: 08:30-12:30</span>
+              <span className="text-neutral-600">
+                :<br className="xs:hidden md:block lg:hidden" /> 08:30-12:30
+              </span>
             </p>
           </li>
         </ul>
