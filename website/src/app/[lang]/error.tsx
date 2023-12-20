85bee8257cabb9a1dc613432cea918ba967caf71
emanuele-hazan <emanuele.hazan@gmail.com>
2023-12-20 14:46:30 +0100

    pagina errore refreshata

--- website/src/app/[lang]/error.tsx
+++ website/src/app/[lang]/error.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function Error({ reset }: { reset: () => void }) {
+  const router = useRouter();
   return (
     <div className="flex bg-neutral-100 justify-center items-center min-h-screen">
       <div className=" text-blacktext-center">
         <h1 className="text-4xl  font-semibold">Something went wrong!</h1>
-        <button className="text-red-500 underline" onClick={() => reset()}>
+        <button
+          className="text-red-500 underline"
+          onClick={() => {
+            router.refresh();
+          }}
+        >
           Try again
         </button>
       </div>
