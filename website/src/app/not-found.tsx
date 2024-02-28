a41770d5381c41957c0440cb034e48a4fe1c0fa7
Simone Tasca <simo.tasca@gmail.com>
2024-02-28 12:15:47 +0100

    better 404

--- website/src/app/not-found.tsx
+++ website/src/app/not-found.tsx
@@ -1,17 +1,35 @@
-function NotFoundPage() {
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import TranslationClientComponent from "@/context/lang/TranslationClientComponent";
+import { generateTranslations } from "@/lib/server/lang";
+
+async function NotFoundPage() {
+  const [translations] = await generateTranslations({
+    header: "misc/header",
+    "mobile-panel": "misc/mobile-panel",
+    search: "misc/search",
+    footer: "misc/footer",
+    contacts: "misc/contacts",
+    auth: "auth",
+  });
+
   return (
-    <div className="min-h-screen bg-white">
-      <div className="grid place-items-center h-[80vh]">
-        <div>
+    <TranslationClientComponent value={translations}>
+      <Header small />
+
+      <div className="grid place-items-center bg-white h-[60vh]">
+        <div className="pb-10">
           <h1 className="text-4xl font-bold text-neutral-800">
-            404 not found!
+            Page not found!
           </h1>
           <a className="text-red-600 underline" href="/">
             back to home
           </a>
         </div>
       </div>
-    </div>
+
+      <Footer />
+    </TranslationClientComponent>
   );
 }
 
