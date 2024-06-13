e256c991ab11deb1e1eb31bb3a5a3252c4f45bee
emanuele-hazan <emanuele.hazan@gmail.com>
2024-06-13 18:53:14 +0200

    loghi nuovi

--- website/src/components/ui/PaymentsBar.tsx
+++ website/src/components/ui/PaymentsBar.tsx
@@ -2,16 +2,20 @@ import iconAmex from "@/images/icons/payment-methods/amex.svg";
 import iconMcard from "@/images/icons/payment-methods/mastercard.svg";
 import iconUpay from "@/images/icons/payment-methods/unionpay.svg";
 import iconVisa from "@/images/icons/payment-methods/visa.svg";
+import iconPaypal from "@/images/icons/payment-methods/paypal.svg";
+import iconKlarna from "@/images/icons/payment-methods/klarna.svg";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 export function PaymentsBar({ className }: { className?: string }) {
   return (
     <div className={twMerge("flex gap-1.5 items-center", className)}>
+      <Image src={iconPaypal} alt="" className="w-9 md:w-11"></Image>
       <Image src={iconVisa} alt="" className="w-5 md:w-7"></Image>
       <Image src={iconMcard} alt="" className="w-5 md:w-7"></Image>
       <Image src={iconAmex} alt="" className="w-5 md:w-7"></Image>
       <Image src={iconUpay} alt="" className="w-5 md:w-7"></Image>
+      <Image src={iconKlarna} alt="" className="w-8 md:w-10"></Image>
     </div>
   );
 }
